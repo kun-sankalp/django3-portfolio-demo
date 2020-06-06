f1a5d8220655275840b7c81e02e2f7793328597f
Kunwar Sankalp Singh <kun.sankalp@gmail.com>
2020-06-06 18:41:41 +0530

    Added Postgress to the project

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -77,8 +77,14 @@ WSGI_APPLICATION = 'personal_portfolio.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        #'ENGINE': 'django.db.backends.sqlite3',
+        #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'demo_portfoliodb',
+        'USER': 'postgres',
+        'PASSWORD': 'django1234',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
