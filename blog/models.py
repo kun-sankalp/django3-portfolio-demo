37b818d56ab5a9a3e0d01f189a4859c662cc7331
Kunwar Sankalp Singh <kun.sankalp@gmail.com>
2020-06-06 13:26:44 +0530

    Working now with responsive website

--- blog/models.py
+++ blog/models.py
@@ -4,3 +4,6 @@ class Blog(models.Model):
     title = models.CharField(max_length=100)
     body = models.TextField()
     pub_date = models.DateField()
+
+    def __str__(self):
+        return self.title
--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
 
-urlpatterns = [
-    path('', views.all_blogs, name='all_blogs')
+app_name = 'blog'
 
+urlpatterns = [
+    path('', views.all_blogs, name='all_blogs'),
+    path('<int:blog_id>/', views.detail, name='detail'),
 ]
